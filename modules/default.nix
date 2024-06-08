07f44bbbceaf9822283487284fec04ec0f630574
installer <installer@gmail.com>
2024-06-09 01:30:02 +1000

    Organise modules

---
+++ modules/default.nix
@@ -0,0 +1,10 @@
+{
+    imports = [
+        ./syst/amd-drivers.nix
+        ./syst/nvidia-drivers.nix
+        ./syst/nvidia-prime-drivers.nix
+        ./syst/intel-drivers.nix
+        ./syst/vm-guest-services.nix
+        ./syst/local-hardware-clock.nix
+    ];
+}
